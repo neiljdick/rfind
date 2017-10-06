fea128f81746b3f940c8b4a90ea975d1882ccf46
Neil Dick <neil@cog.systems>
2017-10-06 21:18:03 +1100

    WIP trying to get arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 extern crate walkdir;
 extern crate regex;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
 
 use walkdir::WalkDir;
 use regex::Regex;
+use structopt::StructOpt;
+use std::env; //get the pwd
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "example", about = "Example of structopt usage.")]
+struct Opt {
+    #[structopt(short = "f", long="fuzzy", help = "Match partial name")]
+    fuzz:bool,
+
+    #[structopt(help ="filename")]
+    needle: String,
+
+    // Optional Directory
+    #[structopt(help = "Directory to search, default to pwd")]
+    directory: Option<String>,
+}
+
 
 fn main() {
-    let re = Regex::new(r"Caddies").unwrap();
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
+    let re = Regex::new(&opt.needle).unwrap();
+
+    match opt.directory {
+        Some(x) => println!("Result: {}", x),
+        None => println!("directory is {:?}",
+                         env::current_dir()
+                         .unwrap()
+                         .to_str()),
+    }
+
     for entry in WalkDir::new("/home/neil/Downloads")
         .into_iter().
         filter_map(|e| e.ok()) {
